c77470f9486de9db034bf965ae1fde1a1f10c192
dayangnurnazihah <dayangziha@gmail.com>
2026-07-05 13:30:18 +0800

    update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { CameraPageComponent } from './pages/camera-page/camera-page.component';
 import { PhotostripComponent } from './pages/photostrip/photostrip.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'camera', pathMatch: 'full' },
-  { path: 'camera', component: CameraPageComponent },
+  { path: '', component: CameraPageComponent },
   { path: 'photostrip', component: PhotostripComponent },
 ];
